424ec72d103637731d98dbd2a5027cedbf3aa9fb
sekar <sekarnara@gmail.com>
2020-01-31 16:00:37 +0700

    hsdgsFG

---
+++ o.js
@@ -0,0 +1,20 @@
+// removeChar=str=>str.split('').slice(1,-1).join('')
+
+// //Playing around with slice
+
+
+// console.log(removeChar('twinkle'))
+
+//NUMBERS BACKWARD SEQUENCE
+
+// var arr=[]
+
+// const reverseSeq = n => {
+// for(var i=n;i<n;i++){
+//     arr.push(n)  
+// }
+// return arr
+// }
+
+// console.log(reverseSeq(10))
+
